5cb5688d5ac563b20514aa3786c4ace657822882
Pawel Piecuch <pawel.piecuch@roche.com>
2021-12-07 11:51:34 +0100

    cleanup build issues; update build detection

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-def can_build(env, platform):
-    return not env["disable_experimental"]
 
+def can_build(env, platform):
+    return env["tools"] and not env["production"] # for testing only
 
 
 def configure(env):
